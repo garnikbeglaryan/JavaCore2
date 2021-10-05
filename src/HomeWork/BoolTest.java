e77e354273a73c8dd4e29486a49ae7d94992c5da
Garnik <Garo_2000>
2021-10-05 17:43:32 +0530

    light.java

---
+++ src/HomeWork/BoolTest.java
@@ -0,0 +1,19 @@
+package HomeWork;
+
+public class BoolTest {
+
+    public static void main(String[] args) {
+
+        boolean b = 23 > 17;
+        b = false;
+        System.out.println("b равно " + b);
+        b = true;
+        System.out.println("b равно " + b);
+
+        if (b) ;
+        System.out.println("Этoт код выполняется.");
+        System.out.println("23 > 17 равно " + (23 > 17));
+
+
+    }
+}
