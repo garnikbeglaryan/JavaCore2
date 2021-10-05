9169eeff169bb4b4aafc321d8e56bd97b00dfc97
Garnik <Garo_2000>
2021-10-05 19:09:18 +0530

    light.java

---
+++ src/HomeWork/Conversion.java
@@ -0,0 +1,29 @@
+package HomeWork;
+
+public class Conversion {
+
+    public static void main(String[] args) {
+
+        byte b;
+        int i = 257;
+        double d = 323.142;
+        System.out.println("Преобразование типа int в тип byte.");
+
+        b = (byte) i;
+
+        System.out.println("i и b " + i + " " + b);
+
+        System.out.println("Преобразование типа douЫe в тип int.");
+
+        i = (int) d;
+
+        System.out.println("d и i " + d + " " + i);
+
+        System.out.println("Преобразование типа douЫe в тип byte.");
+
+        b = (byte) d;
+
+        System.out.println("d и  b " + d + " " + b);
+
+    }
+}
