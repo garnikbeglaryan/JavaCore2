39383e96805ad32a66da5c9f9b17f7d417cc9998
Garnik <Garo_2000>
2021-10-07 22:09:20 +0530

    light.java

---
+++ src/HomeWork/FigurePainter.java
@@ -0,0 +1,21 @@
+package HomeWork;
+
+public class FigurePainter {
+
+    public static void main(String[] args) {
+
+     int i;
+     int j;
+
+        for ( i = 0; i < 5; i++) {
+            for (j= 0; j<i; j++) {
+                System.out.print("* ");
+
+
+            }
+            System.out.println();
+        }
+
+        }
+    }
+
