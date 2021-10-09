a71dced2a54a697f037613ab645827f2c523e42f
Garnik <Garo_2000>
2021-10-09 15:45:26 +0530

    light.java

--- src/HomeWork/FigurePainter.java
+++ src/HomeWork/FigurePainter.java
@@ -4,14 +4,22 @@ public class FigurePainter {
 
     public static void main(String[] args) {
 
-        int i;
-        int j;
-        for(i=0;i<4;i++){
-            for(j=i;j<4;j++)
-
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j <=4-i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k < i+1; k++) {
                 System.out.print("* ");
-
+            }
             System.out.println();
         }
+
     }
+
 }
+
+
+
+
+
+
