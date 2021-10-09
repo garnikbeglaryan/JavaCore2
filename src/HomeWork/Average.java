acc40f2b61d01a29d5e8fa92d218278d77c7fb4c
Garnik <Garo_2000>
2021-10-09 23:15:02 +0530

    light.java

--- src/HomeWork/Average.java
+++ src/HomeWork/Average.java
@@ -4,16 +4,17 @@ public class Average {
 
     public static void main(String[] args) {
 
-        double nums[] = {10.1, 11.2, 12.3, 13.4, 14.5};
 
-        double result = 0;
 
-        int i;
-
-        for (i = 0; i < 5; i++)
-            result = result + nums[i];
-
-        System.out.println("Среднее значение равно " + result / 5);
+            for (int i = 0; i <4; i++) {
+                for (int j =1; j <= 4 - i; j++) {
+                    System.out.print(" " + "*");
+                }
+                System.out.println();
+                for (int k = 3; k <= 3 + i; k++) {
+                    System.out.print(" ");
+                }
+            }System.out.println();
 
     }
 }
--- src/HomeWork/Matrix.java
+++ src/HomeWork/Matrix.java
@@ -4,19 +4,66 @@ public class Matrix {
 
     public static void main(String[] args) {
 
-        double m[][] = {
 
-                {0 * 0, 1 * 0, 2 * 0, 3 * 0},
-                {0 * 1, 1 * 1, 2 * 1, 3 * 1},
-                {0 * 2, 1 * 2, 2 * 2, 3 * 2},
-                {0 * 3, 1 * 3, 2 * 3, 3 * 3}
-        };
-        int i, j;
-        for (i = 0; i < 4; i++) {
-            for (j = 0; j < 4; j++)
-                System.out.print(m[i][j] + "    "   );
+     /*
+
+       int i;
+        int j;
+
+        for ( i = 0; i < 4; i++) {
+            for (j= 0; j<i+1; j++) {
+                System.out.print("* ");
+
+
+            }
             System.out.println();
-        }
+     */
+
+
+
+       /*
+
+          for (int i = 0; i < 4; i++) {
+            for (int j = 1; j <=4-i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k < i+1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        */
+
+
+
+        /*
+
+        int i;
+        int j;
+        for (i = 0; i < 4; i++) {
+            for (j = i; j < 4; j++)
+
+                System.out.print("* ");
+
+            System.out.println();
+       */
+
+
+
+        /*
+
+        for (int i = 0; i <4; i++) {
+            for (int j =1; j <= 4 - i; j++) {
+                System.out.print(" " + "*");
+            }
+            System.out.println();
+            for (int k = 3; k <= 3 + i; k++) {
+                System.out.print("  ");
+            }
+        }System.out.println();
+
+        */
     }
 }
+
+
 
