6a5475d7c5aa48b42877ee36f41aaa1494fec565
Garnik <Garo_2000>
2021-10-13 16:26:45 +0530

    Light.java

--- src/HomeWork2/FigurePainter2.java
+++ src/HomeWork2/FigurePainter2.java
@@ -4,13 +4,92 @@ public class FigurePainter2 {
 
     public static void main(String[] args) {
 
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+
+
+        System.out.println("\n orinak 2");
+
+
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j <=i ; j++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
+
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+
+
+        System.out.println("\n orinak 3");
+
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = i; j < 4; j++) {
+                System.out.print(" *");
+
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+        System.out.println("\n orinak 4");
+
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+
+            }
+
+            for (int k = i; k < 4; k++) {
                 System.out.print("* ");
 
             }
             System.out.println();
         }
 
+
+        System.out.println();
+
+
+        System.out.println("\norinak 5");
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
+
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("  * ");
+
+            }
+            System.out.println();
+
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 1; j <=3-i; j++) {
+                System.out.print("    "+"*");
+            }System.out.println();
+            for (int k = 3; k <= 3+i; k++) {
+                System.out.print(" ");
+            }
+        }System.out.println();
     }
 }
+
+
