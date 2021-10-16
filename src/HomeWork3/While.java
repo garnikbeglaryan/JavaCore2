0d6f889e96755964e9dc23bb6584d16cb8c49a23
Garnik <Garo_2000>
2021-10-16 12:02:00 +0530

    Light.java

---
+++ src/HomeWork3/While.java
@@ -0,0 +1,14 @@
+package HomeWork3;
+
+public class While {
+
+    public static void main(String[] args) {
+
+        int n = 10 ;
+        while (n > 0){
+            System.out.println("тaкт " + n);
+            n--;
+        }
+
+    }
+}
