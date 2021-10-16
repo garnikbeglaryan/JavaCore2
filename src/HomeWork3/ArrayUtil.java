1b5d5e3c7a17fc60d56f80d4d99d638e68281e52
Garnik <Garo_2000>
2021-10-16 23:47:48 +0530

    Light.java

---
+++ src/HomeWork3/ArrayUtil.java
@@ -0,0 +1,68 @@
+package HomeWork3;
+
+public class ArrayUtil {
+
+    public static void main(String[] args) {
+
+        int [] array = {2,5,8,10,32,22,11,66,12,30};
+
+        // Տպել մասիվի բոլոր էլեմենտները,
+
+//        int[]array={2,5,8,10,32,22,11,66,12,30};
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println(array[i]);
+
+
+        //Տպել մասիվի բոլոր զույգ էլեմենտները,
+
+//        int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i]%2 == 0){
+//                System.out.println(array[i]);
+
+
+        //Տպել մասիվի բոլոր կենտ էլեմենտները
+
+//        int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0){
+//                System.out.println(array[i]);
+
+
+
+        //Տպել զույգերի քանակը
+
+//        int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
+//        int caunte = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i]%2 == 0){
+//              caunte++;
+
+
+
+        //Տպել կենտերի քանակը
+//
+//        int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
+//        int count = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0){
+//                count++;
+
+
+
+
+        //Տպել մասիվի բոլոր թվերի միջին թվաբանականը
+//
+//        int[]array={2,5,8,10,32,22,11,66,12,30};
+//        double gumar = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            gumar += array[i];
+//        }
+//        System.out.println(gumar);
+//        System.out.println(gumar /array.length);
+
+
+
+
+    }
+}
