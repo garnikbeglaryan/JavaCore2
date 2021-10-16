7d851db9409f844492087b556ee42068920e86b9
Garnik <Garo_2000>
2021-10-16 11:58:17 +0530

    Light.java

---
+++ src/HomeWork3/StringSwitch.java
@@ -0,0 +1,24 @@
+package HomeWork3;
+
+public class StringSwitch {
+
+    public static void main(String[] args) {
+
+        String str = "два";
+        switch (str) {
+            case "один":
+                System.out.println("oдин");
+                break;
+            case "два ":
+                System.out.println("два");
+                break;
+            case "три":
+                System.out.println("три");
+                break;
+            default:
+                System.out.println(("нe совпало)");
+                break;
+        }
+
+    }
+}
