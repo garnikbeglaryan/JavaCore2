023fe3947b95fd097d77ab97556216643971d1aa
Garnik <Garo_2000>
2021-10-19 17:43:54 +0530

    Light.java

---
+++ src/HomeWork4/ArrayUtil2.java
@@ -0,0 +1,126 @@
+package HomeWork4;
+
+public class ArrayUtil2 {
+
+    public static void main(String[] args) {
+
+        //Տպեք մասիվի բոլոր էլեմենտները
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.print(array[i] + " ");
+//
+//        }
+
+
+
+
+
+
+        //Տպեք մասիվի ամենամեծ թիվը
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        int max = array[0];
+//        for (int i = 1; i < array.length; i++) {
+//            if (array[i] > max) ;
+//            max = array[i];
+//
+//        }
+//
+//        System.out.println("max = " + max);
+
+
+
+
+
+        //Տպեք մասիվի ամենափոքրը թիվը
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        int min = array[0];
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] < min)
+//            min = array[i];
+//
+//        }
+//        System.out.println("min =" + min);
+
+
+
+
+
+
+        //Տպեք մասիվի բոլոր զույգ էլեմենտները
+
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 == 0)
+//                System.out.println(array[i]);
+//
+//        }
+
+
+        //Տպեք մասիվի բոլոր կենտ էլեմենտները
+
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0)
+//                System.out.println(array[i]);
+//
+//        }
+
+
+
+
+
+        //Տպեք զույգերի քանակը
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        int count = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 == 0) {
+//                count++;
+//
+//            }
+//
+//        }
+//        System.out.println(count);
+
+
+
+
+
+
+        //Տպեք կենտերի քանակը
+//
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        int count = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0) {
+//                count++;
+//
+//            }
+//        }
+//        System.out.println(count);
+
+
+
+
+
+
+        //Տպեք մասիվի բոլոր թվերի միջին թվաբանականը
+        //Տպեք մասիվի էլեմենտների գումարը։
+
+//        int[] array = {2, 6, 12, 24, 16, 17, 18, 45, 62, 47};
+//        double money = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            money += array[i];
+//
+//        }
+//        System.out.println(money);
+//        System.out.println(money / array.length);
+
+
+    }
+}
