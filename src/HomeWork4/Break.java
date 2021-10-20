d7740553e4ebcfa031b96b5bbb73fcbe925a14e1
Garnik <Garo_2000>
2021-10-20 10:39:58 +0530

    light.java

---
+++ src/HomeWork4/Break.java
@@ -0,0 +1,25 @@
+package HomeWork4;
+
+public class Break {
+
+    public static void main(String[] args) {
+
+        boolean t = true;
+
+        first : {
+            second : {
+                third : {
+                    System.out.println("Предшествует оператору break.");
+                    if (t) break second ;
+                    System.out.println("Этот оператор не будет выполняться");
+                }
+                System.out.println("Этот оператор не будет выполняться");
+            }
+            System.out.println("Этот оператор следует за блоком second.");
+        }
+
+        }
+
+
+
+    }
