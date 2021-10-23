105d5ce9f48bfc6bc0378a1fa03d4dbb145ab953
Garnik <Garo_2000>
2021-10-23 22:22:33 +0530

    light.java

---
+++ src/HomWork5/CharArray.java
@@ -0,0 +1,73 @@
+package HomWork5;
+
+public class CharArray {
+
+    public static void main(String[] args) {
+
+
+        //Պետք է գրել լոգիկա, որը կհաշվի թե քանի հատ կա c-փոփոխականից մեր մասիվի մեջ, ու տպել այդ քանակը։
+
+
+//        char[] bolola = {'b', 'o', 'l', 'o', 'l', 'a'};
+//        int count = 0;
+//        for (int i = 0; i < bolola.length; i++) {
+//            char c = bolola[i];
+//            if (c == 'o') {
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+
+
+
+
+
+
+        //Գրել կոդ, որը կտպի մեջտեղի 2 սինվոլները։
+
+
+//        char[] bolola = {'b', 'o', 'l', 'o', 'l', 'a'};
+//        System.out.println(bolola[bolola.length /2 -1]);
+//        System.out.println(bolola[bolola.length / 2]);
+
+
+
+
+
+
+
+        // Տպենք true եթե մեր մասիվը վերջանում է ly-ով, եթե ոչ՝ false
+
+
+//        char[] bolola = {'b', 'o', 'l', 'o', 'l', 'a'};
+//        if (bolola[bolola.length - 2] == 'l' && bolola[bolola.length - 1] == 'y') {
+//            System.out.println(true);
+//
+//        } else
+//            System.out.println(false);
+
+
+
+
+
+
+        //Տպել true եթե տեքստը պարունակում է bob բառը
+
+
+//        char[] bolola = {'b', 'a', 'b', 'o', 'l', 'a'};
+//        for (int i = 0; i < bolola.length; i++) {
+//            if (bolola[i] == 'b' && bolola[i + 2] == 'b') {
+//                System.out.println(true);
+//                  break;
+//            }else
+//                System.out.println(false);
+//                  break;
+//       }
+
+
+
+
+
+
+    }
+}
